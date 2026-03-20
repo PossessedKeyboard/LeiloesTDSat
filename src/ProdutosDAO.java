ee1d43d920fa629a174f2ff3fbd6eb7caeb835cc
PossessedKeyboard <mollolucas@gmail.com>
2026-03-20 00:40:45 -0300

    Botão de salvamento de cadastro

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,23 +12,40 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
     
-    Connection conn;
+    Connection        conn;
     PreparedStatement prep;
-    ResultSet resultset;
+    ResultSet    resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
         
-        
+        //int status;
+        try {
+
+            prep = conn.prepareStatement("INSERT INTO produtos(nome,valor) VALUES(?,?)");
+            prep.setString(1,produto.getNome());
+            prep.setInt(2,produto.getValor());
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null,"Sucesso!");
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            JOptionPane.showMessageDialog(null,"Não foi possível inserir os dados! Por favor, verifique valores digitados!");
+           // return ex.getErrorCode();
+        }
     }
+        
+        
+        
+    
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -21,13 +21,21 @@ public class conectaDAO {
         Connection conn = null;
         
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root","fr33B1RD");
             
-        } catch (SQLException erro){
+        } catch (ClassNotFoundException |SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
+    }
+    
+    public void disconnectDB(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+
+        }
     }
     
 }
